ad60a3a1a9dcd15653ee24c9387d1cf24a010640
GowNanSA <69057236+GowNanSA@users.noreply.github.com>
2025-03-03 15:51:14 -0600

    So far progress on enemy script

--- Assets/Scripts/EnemyType1AI.cs
+++ Assets/Scripts/EnemyType1AI.cs
@@ -1,18 +1,65 @@
 using UnityEngine;
+using UnityEngine.AI; // for built in stuff 
+/// <summary>
+/// REFERENCE USED https://youtu.be/UjkSFoLxesw?feature=shared
+/// </summary>
 // enemy type 1 
 
 
 public class EnemyType1AI : MonoBehaviour
 {
+    public Transform playerTarget;
+
+    public NavMeshAgent agent;
+
+    public LayerMask ground1, player1;
+
+    // to allow the enemy to patrol the player 
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // to allow enemy to attack should deplete health like... every 3 seconds of touch or smth 
+    public float timeBeforeAttack;
+
+
+
+    // state machine reference
+    public float sightRange, attackRange; // player in sight 
+    bool playerSeen, playerAttack; // bools to check if player is in range or in sight
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // search for the player 
+        playerTarget = GameObject.Find("Player").transform;
+        // assign navmesh agent 
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // check if the player is in sight or range 
+        // check if in sight or attack possible
+        playerSeen = Physics.CheckSphere(transform.position, sightRange, player1); // player is seen or not 
+        playerAttack = Physics.CheckSphere(transform.position, attackRange, player1); // player can be attacked or not 
+
+
+        if (!playerSeen && !playerAttack) Patrol();
+        if (!playerSeen && !playerAttack) Chase();
+        if (playerSeen && playerAttack) Attack();
+    }
+
+
+
+    // patrol 
+    void Patrol() {
+      
+    }
+
+    // chase and attack 
+    void Chase() { 
+
     }
 }
