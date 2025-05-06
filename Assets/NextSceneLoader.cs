1b96c4ff12e07c467aaa77bf0bb81d0066dda703
Leah Joshua <leahjoshua1313@gmail.com>
2025-05-05 20:57:14 -0500

    finish intro + connect scenes

---
+++ Assets/NextSceneLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneLoader : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+}
