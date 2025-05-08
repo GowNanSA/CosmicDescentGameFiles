6a16460044e472962e04171b1e788602df7c5f5c
Leah Joshua <leahjoshua1313@gmail.com>
2025-05-07 23:41:38 -0500

    add start screen and fix end game

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        // Deselect any UI element on scene load
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
     public void OpenScene(int index) {
         SceneManager.LoadScene(index);
     }
